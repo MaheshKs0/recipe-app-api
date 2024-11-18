b3c978841f24804d8280493fd980b89a0dc3b085
Mahesh Ks <ksmahesh0608@gmail.com>
2024-11-17 19:02:49 -0700

    wait for db,changes to docker and check.yml

--- app/app/calc.py
+++
@@ -1,9 +0,0 @@
-"""calculator functions"""
-
-def add(x,y):
-    """Add x and y and return result"""
-    return x+y
-
-def subtract(x,y):
-    """subtract y from x and retrun result"""
-    return x-y
